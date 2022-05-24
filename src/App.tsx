c90347a0da234a865b7509681698766d7aa05ba1
raimax <4miner44@gmail.com>
2022-05-25 00:36:14 +0300

    new routes: signin, signup, newpost

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
+import NewPostPage from "./pages/NewPostPage/NewPostPage";
 import PostPage from "./pages/PostPage/PostPage";
+import SigninPage from "./pages/SigninPage/SIgninPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/posts/:postId" element={<PostPage />} />
+          <Route path="/newpost" element={<NewPostPage />} />
         </Routes>
       </div>
     </BrowserRouter>
